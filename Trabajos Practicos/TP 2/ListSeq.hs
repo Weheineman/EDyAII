62d8bf43102a1ff345c63fb4e01d7189610ccb85
Gianni Weinand <gi4nn1@yahoo.de>
2018-06-18 14:41:17 -0300

    Miniavance tp2

---
+++ Trabajos Practicos/TP 2/ListSeq.hs
@@ -0,0 +1,41 @@
+{- Implementación del TAD secuencia -}
+
+module Seq where
+
+class Seq s where
+   emptyS     :: s a
+   singletonS :: a -> s a
+   lengthS    :: s a -> Int 
+   nthS       :: s a -> Int -> a 
+   tabulateS  :: (Int -> a) -> Int -> s a
+   mapS       :: (a -> b) -> s a -> s b 
+   filterS    :: (a -> Bool) -> s a -> s a 
+   appendS    :: s a -> s a -> s a
+   takeS      :: s a -> Int -> s a
+   dropS      :: s a -> Int -> s a
+   showtS     :: s a -> TreeView a (s a)
+   showlS     :: s a -> ListView a (s a)
+   joinS      :: s (s a) -> s a
+   reduceS    :: (a -> a -> a) -> a -> s a -> a
+   scanS      :: (a -> a -> a) -> a -> s a -> (s a, a)
+   fromList   :: [a] -> s a
+
+data TreeView a t = EMPTY | ELT a | NODE t t
+data ListView a t = NIL | CONS a t
+
+instance Seq [] where
+    emptyS = []
+    singletonS x = [x]
+    lengthS xs = length xs
+    nthS (x:xs) n
+        | n==0         = x
+        | otherwise = nthS xs (n-1)
+    {-Hay que paralelizar-}
+    tabulateS f n = []
+    mapS = map
+    filterS = filter
+    appendS = 
+    takeS = take
+    dropS = drop
+    fromList xs = xs
+    
