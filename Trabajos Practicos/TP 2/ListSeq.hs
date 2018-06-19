2bd115d8fae4b242d66b0732ecbb3573cfa73324
Weheineman <gi4nn1@yahoo.de>
2018-06-19 14:06:41 -0300

    Avance en ListSeq, se rompe por tipos ambiguos

--- Trabajos Practicos/TP 2/ListSeq.hs
+++ Trabajos Practicos/TP 2/ListSeq.hs
@@ -1,19 +1,53 @@
 {- Implementación del TAD secuencia -}
+module ListSeq where
+
 import Seq
 import Par
 
+
+--Genera la lista al reves (pues agregar al principio es mas eficiente)
+auxTabulateS :: (Int -> a) -> Int -> [a]
+auxTabulateS f 0 = [f 0]
+auxTabulateS f n = x:xs
+    where (x,xs) = (f n) ||| (auxTabulateS f (n-1))
+
 instance Seq [] where
     emptyS = []
+    
     singletonS x = [x]
+    
     lengthS xs = length xs
+    
     nthS (x:xs) n
-        | n==0         = x
+        | n==0      = x
         | otherwise = nthS xs (n-1)
-    {-Hay que paralelizar-}
-    tabulateS f n = []
-    mapS = map
-    filterS = filter
+    
+    tabulateS f n    = reverse $ auxTabulateS f (n-1)
+    
+    mapS f []        = []
+    mapS f (x:xs)    = y:ys
+        where (y,ys) = (f x) ||| (mapS f xs)
+    
+    filterS f [] = []
+    filterS f (x:xs)
+        | check     = x:ys
+        | otherwise = ys
+      where (check, ys) = (f x) ||| (filterS f xs)
+    
+    appendS = (++)
+    
     takeS xs n = take n xs
+    
     dropS xs n = drop n xs
+    
+    showtS []   = EMPTY
+    showtS [x]  = ELT x
+    --No se como dividirlo pls askerino
+    showtS xs   = NODE (takeS xs l) (dropS xs l)
+      where l = quot (lengthS xs) 2
+    
+    showlS []       = NIL
+    showlS (x:xs)   = CONS x xs
+        
     fromList xs = xs
     
